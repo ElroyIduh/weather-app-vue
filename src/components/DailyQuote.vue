4bdc9540bad7d9b6b530da1a720fb5c256a8c0cb
Elroy Iduh <elroy1191990@gmail.com>
2021-06-24 22:26:04 +0200

    failed

---
+++ src/components/DailyQuote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text">
+      <h1>Daily Quote</h1>
+        <p>{{ text }}</p>
+        
+  </div>
+
+  <div class="author">
+      <h2><p>{{ author }}</p></h2>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["text", "author"]
+
+}
+</script>
+
+<style>
+
+</style>
