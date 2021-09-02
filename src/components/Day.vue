c1e7b5eb3074e10a64b3abc98e4a5cb420333e2a
Elroy Iduh <elroy1191990@gmail.com>
2021-09-02 22:14:15 +0200

    hour/ cards

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,79 +1,85 @@
 <template>
-
-<!-- Cards und Card Input -->
+  <!-- Cards und Card Input -->
   <div class="card">
-  <img :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" :alt="day.weather[0].description">
-  <div >
-    <p>{{ getDay(day.dt) }} - {{ getDate(day.dt) }}</p>
-    <p><i class="fas fa-temperature-low"></i> {{ formatTermperature(day.temp.min) }}</p>
-    <p><i class="fas fa-temperature-high"></i> {{ formatTermperature(day.temp.max) }}</p>
-    <p><i class="fas fa-wind"></i> {{ formatWindspeed(day.wind_speed) }}</p>
-    <p><i class="fas fa-humidity"></i> {{ formatHumidity(day.humidity) }} %</p>
+    <img
+      :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+      :alt="day.weather[0].description"
+    />
+    <div>
+      <p>{{ getDay(day.dt) }} - {{ getDate(day.dt) }}</p>
+      <p>
+        <i class="fas fa-temperature-low"></i>
+        {{ formatTermperature(day.temp.min) }}
+      </p>
+      <p>
+        <i class="fas fa-temperature-high"></i>
+        {{ formatTermperature(day.temp.max) }}
+      </p>
+      <p><i class="fas fa-wind"></i> {{ formatWindspeed(day.wind_speed) }}</p>
+      <p>
+        <i class="fas fa-humidity"></i> {{ formatHumidity(day.humidity) }} %
+      </p>
 
-    <p v-if="day.rain"><i class="fas fa-cloud-rain"></i> {{ formatRain(day.rain) }}</p>
-   
+      <p v-if="day.rain">
+        <i class="fas fa-cloud-rain"></i> {{ formatRain(day.rain) }}
+      </p>
+    </div>
   </div>
-</div>
-
-
 </template>
 
 <script>
-
 export default {
   props: ["day"],
- methods:{
+  methods: {
     getDate(dt) {
-            const date = new Date(dt * 1000);
-            
-            const month = "0" + (date.getMonth() + 1);
-            const day = "0" + date.getDate();
-            const year = date.getFullYear();
+      const date = new Date(dt * 1000);
 
-            const formattedDate =  day.substr(-2) + '.' + month.substr(-2) + '.' + year;
+      const month = "0" + (date.getMonth() + 1);
+      const day = "0" + date.getDate();
+      const year = date.getFullYear();
 
-            return formattedDate;
-        },
-        getDay(dt) {
-          const days = ["SO","MO","DI","MI","DO","FR","SA"];
+      const formattedDate =
+        day.substr(-2) + "." + month.substr(-2) + "." + year;
 
-          const date = new Date(dt * 1000)
+      return formattedDate;
+    },
+    getDay(dt) {
+      const days = ["SO", "MO", "DI", "MI", "DO", "FR", "SA"];
 
-          const dayNumber = date.getDay()
-          return days[dayNumber]
-        },
-        formatTermperature(temp) {
-          const roundedTemp = Math.round(temp)
-          return`${roundedTemp} °C`
-        },
-        formatRain(rain) {
-          const rainProbability = rain
-          return` ${rainProbability} %`
-        },
-        formatWindspeed(wind_speed) {
-          
-          const windSpeed = Math.round((wind_speed * 3.6) * 10) /10
-          return `${windSpeed} km/h`
-        },
-        formatHumidity(humidity) {
-          const humidityLevel = humidity
-          return ` ${humidityLevel}`
-        },
+      const date = new Date(dt * 1000);
 
- }
-}
+      const dayNumber = date.getDay();
+      return days[dayNumber];
+    },
+    formatTermperature(temp) {
+      const roundedTemp = Math.round(temp);
+      return `${roundedTemp} °C`;
+    },
+    formatRain(rain) {
+      const rainProbability = rain;
+      return ` ${rainProbability} %`;
+    },
+    formatWindspeed(wind_speed) {
+      const windSpeed = Math.round(wind_speed * 3.6 * 10) / 10;
+      return `${windSpeed} km/h`;
+    },
+    formatHumidity(humidity) {
+      const humidityLevel = humidity;
+      return ` ${humidityLevel}`;
+    },
+  },
+};
 </script>
 
 <style>
 .card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   width: 40%;
   border-radius: 5px;
-  
 }
 .card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 /* .container {
   padding: 2px 16px;
--- src/components/Hour.vue
+++ src/components/Hour.vue
@@ -1,15 +1,18 @@
 <template>
+<div class = "card-hour">
   <div v-if="hour" class="hour">
-    <div>{{ getTime(hour.dt) }}</div>
-    <div>
-      {{ hour.weather[0].description }}
-    </div>
-    <div>{{ formatTermperature(hour.temp) }}</div>
-
     <img
       :src="`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`"
       :alt="hour.weather[0].description"
     />
+    <p>{{ getTime(hour.dt) }}</p>
+    <p>
+      {{ hour.weather[0].description }}
+    </p>
+    <p>{{ formatTermperature(hour.temp) }}</p>
+
+   
+  </div>
   </div>
 </template>
 
@@ -44,6 +47,19 @@ export default {
   outline-style: inherit;
   outline-color: ivory;
   align-items: center;
+  
 }
 
+
+.card-hour {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+  margin-right: 2rem;
+  
+}
+.card-hour:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
