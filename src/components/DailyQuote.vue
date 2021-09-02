87f5481e1f17e17ead1506d3b6be2ce4cbf21c63
Elroy Iduh <elroy1191990@gmail.com>
2021-09-02 22:09:28 +0200

    optik dailyquote angepasst

--- src/components/DailyQuote.vue
+++ src/components/DailyQuote.vue
@@ -1,45 +1,43 @@
 <template>
   <div class="card quote">
     <div>
-      <h2>{{ text }}</h2>
-      <h3>{{ author }}</h3>
-  </div> 
+      <h3>{{ text }}</h3>
+      <h4>{{ author }}</h4>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ["text", "author"],
-    data() {
-      return{
-        quote: null,
-      }
-    },
-    created() {
-      this.getDailyQuote()
+  props: ["text", "author"],
+  data() {
+    return {
+      quote: null,
+    };
+  },
+  created() {
+    this.getDailyQuote();
+  },
+  methods: {
+    getDailyQuote() {
+      const url = "https://quotes.rest/qod";
 
+      fetch(url).then((response) => {
+        console.log(response);
+      });
     },
-    methods: {
-      getDailyQuote() {
-        const url = "https://quotes.rest/qod"
-
-        fetch(url)
-        .then(response => {
-          console.log(response)
-        })
-      }
-    }
-}
+  },
+};
 </script>
 
 <style>
-  .quote {
-    color:hsl(0, 0%, 40%);
-    font-style: italic;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.quote {
+  color: hsl(0, 0%, 40%);
+  font-style: italic;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 /*
   element
   .class
@@ -51,21 +49,21 @@ export default {
   padding: 2rem;
 }
 
-  .quote .text {
-    font-size: 1,5rem;
-  }
+.quote .text {
+  font-size: 1, 5rem;
+}
 
-  .quote .text {
-    font-size: 0,9rem;
-  }
+.quote .text {
+  font-size: 0, 9rem;
+}
 
-  .card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   width: 40%;
   border-radius: 5px;
 }
 .card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
